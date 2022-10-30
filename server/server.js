7a7fc7f49bf4188c93844849c989f239aa5c52b8
Aman Pandagre <amanpandagre21@gmail.com>
2022-10-30 14:04:10 +0530

    debug code

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const { connectDatabase } = require("./config/Database/database");
-const cloudinary = require("cloudinary");
+const cloudinary = require("cloudinary").v2;
 // port number
 const PORT = process.env.PORT || 5000;
 
