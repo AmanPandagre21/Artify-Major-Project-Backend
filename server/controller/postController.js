fb63de4cb57f30524a467b582c7322a3b510a0b2
Aman Pandagre <amanpandagre21@gmail.com>
2022-11-11 00:44:13 +0530

    test ml modelm

--- server/controller/postController.js
+++ server/controller/postController.js
@@ -23,20 +23,21 @@ exports.createPost = async (req, res, next) => {
     if (!title || !description || !category) {
       return next(new ErrorHandler("Required Field", 400));
     }
-    console.log(req.body.imgUrl);
+    console.log(imgUrl);
     model
       .classify({
         imageUrl: imgUrl,
       })
       .then((predictions) => {
         console.log(predictions);
-        predict = [...predictions];
-        console.log(predict);
-        // res.json({
-        //   code: 200,
-        //   status: "success",
-        //   predictions,
-        // });
+        // predict = [...predictions];
+        // console.log(predict);
+        res.json({
+          code: 200,
+          status: "success",
+          predictions,
+        });
+        next();
       })
       .catch((e) => {
         console.log("error", e);
