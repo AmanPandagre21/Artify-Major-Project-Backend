61121b810dda11f92c7bff75dc78d114464eb2cf
Aman Pandagre <amanpandagre21@gmail.com>
2022-11-27 14:10:11 +0530

    fix contact field

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -280,7 +280,10 @@ exports.updateUserProfile = async (req, res, next) => {
 
     if (name) user.name = name;
 
-    if (number) user.phone = Number(number);
+    if (number) {
+      if (validator.isMobilePhone(number.toString(), "en-IN"))
+        user.phone = Number(number);
+    }
 
     if (bio) user.bio = bio;
 
